4a1e5a17b9bf81ee3cc757f26f2270583b4966ab
jdbalantar <jdbalantar@gmail.com>
2022-04-08 15:16:43 -0500

    Ejercicio de la sesion 3;

---
+++ Sesion3/Sesion3/Program.cs
@@ -0,0 +1,24 @@
+﻿Cliente cliente = new Cliente("Juan David", 3005241436, "Mi dirección en COLOMBIA", "correo@gmail.com", true);
+Console.WriteLine($"Información del cliente: {cliente}");
+Console.ReadKey();  
+
+public struct Cliente
+{
+    public Cliente(string nombre, long telefono, string direccion, string email, bool isNew)
+    {
+        Nombre = nombre;
+        Telefono = telefono;
+        Direccion = direccion;
+        Email = email;
+        IsNew = isNew;
+
+    }
+
+    public string Nombre { get; set; }
+    public long Telefono { get; set; }
+    public string Direccion { get; set; }
+    public  string Email { get; set; }
+    public bool IsNew { get; set; }
+
+    public override string ToString() => $"{Nombre}, {Telefono}, {Direccion}, {Email}, {IsNew}";
+}
