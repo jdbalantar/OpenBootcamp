47fc2401e50119abb8fbe5b913ef940e48dae354
jdbalantar <jdbalantar@gmail.com>
2022-04-08 10:55:54 -0500

    Ejercicios de la sesión 1;

---
+++ Sesion1/Sesion1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("\t\t\t\t\t\t*****Ejercicio #1*****");
+Console.WriteLine("Escribe tu nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine("¡Mucho gusto, " + nombre + "!");
+Console.ReadKey();
+
+Console.WriteLine("\t\t\t\t\t\t*****Ejercicio #2*****");
+string date = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine("Son las " + date);
+Console.ReadKey();
